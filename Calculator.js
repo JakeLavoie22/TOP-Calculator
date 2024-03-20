a2b7df6c38313416bb9b42dcf90b952075622623
TheNewJakeLavoie <119072252+JakeLavoie22@users.noreply.github.com.>
2024-03-20 09:31:49 -0400

    Feat: back-end logic complete and cleaned-up based on current know-how. Calculator fully functionnal

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,4 @@
-// Front-End JS 
+// Front-End JS (In hind sight, should have been mostly  html )
 
 const allDigitInfo = [];                 //Buttons are pushed to this array in "calcButtonsGenerator"
 const extraButtonInfo = [               // The info from this array is pushed to the buttons objects in "pushInfoToButtons"
@@ -42,8 +42,8 @@ const calcButtonsGenerator = (function() {                        // Main functi
 function generateButton(number, buttonClass, buttonsBody){ // buttonsBody constant is defined in IIFE function line 27, need to add it as third parameter in order to access outside it the function 
     for (let i = 1; i <= number; i++) {
         let button = document.createElement("button");  
-        button.type = "button";    // this is to avoid "form-filling" when button pressed 
-        button.classList.add(buttonClass);     //add CSS class for each button element created 
+        button.type = "button";                        // this is to avoid "form-filling" when button pressed 
+        button.classList.add(buttonClass);            //add CSS class for each button element created 
         buttonsBody.appendChild(button);
         allDigitInfo.push({button});
     };
@@ -58,12 +58,11 @@ const totalButtons = allDigitInfo.length;  // number of buttons is the length of
         allDigitInfo[i].button.textContent = extraButtonInfo[i].btnText; 
     };
 })(allDigitInfo, extraButtonInfo);
-//console.table(extraButtonInfo);
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Back-end Logic : 
-
 
 
 
@@ -73,14 +72,6 @@ const numberBtns = allDigitInfo.filter((buttonInfo) => {   //filter out digit bu
     }
 });
 
-
-const workBtns = allDigitInfo.filter((buttonInfo) => {   //filter out the work buttons from allDigitInfo 
-    if(buttonInfo.button.value == "undefined" && buttonInfo.button.textContent !== "."){
-        return true;
-    }
-});
-
-
 const resetBtn = allDigitInfo[0].button;
 const signChangeBtn = allDigitInfo[1].button;
 const factorialBtn = allDigitInfo[2].button;
@@ -89,95 +80,57 @@ const multiplyBtn = allDigitInfo[7].button;
 const minusBtn = allDigitInfo[11].button;
 const plusBtn = allDigitInfo[15].button;
 const comaBtn = allDigitInfo[17].button;
-
-comaBtn.value = ".";
 const equalBtn = allDigitInfo[18].button; 
 
-const operatorBtns = [plusBtn,  multiplyBtn, minusBtn, divideBtn];
+const operatorBtns = [plusBtn,  multiplyBtn, minusBtn, divideBtn, equalBtn];
+const specialBtns = [factorialBtn, signChangeBtn]
 
 let displayValue = 0;
-
 let result = 0;
-
+let opClicked = "false";
 
 (function build(){
 
-let opClicked = "false"; //no operator button has been clicked yet 
-let a = "";  
-let b = ""; 
-let opClickCount = 0;
-let lastClick = "=";
-let currentClick;
+let a = "";   // first operand 
+let b = "";   // second operand
+let opClickCount = 0;    // number of times an operator was clicked, reset by  "=" and "A/C" buttons
+let lastClick = "";     
+let bClicked; 
 
-function updateDisplay() {
-    const display = document.querySelector(".screenBody");
-    display.innerText = displayValue;
-    if(displayValue.length > 9) {
-        display.innerText = displayValue.substring(0, 9);
-        console.log(displayValue.substring(0, 9));
-    };
-};
+
 
 numberBtns.forEach(buttonInfo => {                       
     buttonInfo.button.addEventListener('click', () => {
+        bClicked = "false";  // this resets all the time. its only used for b operand coma ( line 193 )          
 
-
-    if(lastClick === "=" || lastClick === "a"){
-        a += buttonInfo.button.value;
-        displayValue = a;
-        lastClick = "a";
-        console.log("a value: ", a, ",last Click is ", lastClick, ", currentClick is ", currentClick);
-
-    } else if (lastClick !== "a") {
-        b += buttonInfo.button.value;
-        displayValue = b;
-        console.log("b value: ", b, ",last Click", lastClick);
-    };
-
-    updateDisplay();
-
-    if(opClickCount > 0 && lastClick !== a ){
-        console.log("computed");
-        if(lastClick === "+" ){
-            result = Number(a) + Number(b);
-            lastClick = "+";
-        } else if(lastClick === "*"){
-            console.log("*");
-            result = Number(a) * Number(b);
-            lastClick = "*";
-        } else if (lastClick === "-"){
-            console.log("-");
-            result = Number(a) - Number(b);
-            lastClick = "-";
-        } else if (lastClick === "/"){
-            if ( b === 0 ){
-                displayValue = "ERROR"
-            };
-            console.log("/");
-            result = Number(a) / Number(b);
-            lastClick = "/";
+        if ((lastClick === "=") && (a.length <= 10)){     //Screen resets if "=" is pressed and a number is entered straight away, without an operator
+            a = buttonInfo.button.value;   // resets screen and calculation 
+            displayValue = a;
+            lastClick = "a";
+        }else if((lastClick === "" || lastClick === "a") && a.length <= 10){     //store numbers clicked into a operand
+            a += buttonInfo.button.value;
+            displayValue = a;
+            lastClick = "a";
+        } else if ((lastClick !== "a") && b.length <= 10){    //store numbers clicked into b operand
+            bClicked = "true";
+            b += buttonInfo.button.value;
+            displayValue = b;
         };
-    }  else {
-        result = displayValue;
-    };
-    console.log("display value is", displayValue, "actual result is ", result, ", a is now", a, ", b is ", b, ", last click is ", lastClick, "clickcount is ", opClickCount);
+    
+        updateDisplay();        
+    
+        // if operator previously clicked, compute a and b calculation based on the operator lastClick, but don't update display it yet 
+        if(opClickCount > 0 && lastClick !== a ){
+            compute(a, b, lastClick);
+            console.log("computed but not displayed yet");
+        }  else {
+            result = displayValue; 
+        }; 
+        console.log("a is now", a," type :", (typeof a),", b is ", b);
+        console.log("you clicked ",buttonInfo.button.textContent ,"button, Display value is", displayValue, "actual result is ", result," ,result type:", (typeof result), );
+        console.log("last click is ", lastClick, ",clickCount is ", opClickCount);
 
-        });
     });
-
-
-
-comaBtn.addEventListener('click', () => {
-    a.toString();
-    b.toString();
-    if (!a.includes(".")){   // removed && a != ""
-        a += comaBtn.value;
-        displayValue = a;
-    } if (!b.includes(".")){   // removed  && b != ""
-        b += comaBtn.value;
-        displayValue = b;
-    }; 
-    updateDisplay();
 });
 
 resetBtn.addEventListener('click', () => {
@@ -187,178 +140,131 @@ resetBtn.addEventListener('click', () => {
     opClickCount = 0;
     lastClick = "=";
     displayValue = 0;
-    currentClick = "";
-    console.log("reset");
     updateDisplay();
-})
+    console.log("reset");
+});
 
-operatorBtns.forEach(button=> {
-    button.addEventListener('click', () => {
-        opClickCount ++;
-        a = result;
-        b = "";
-        displayValue = result.toString();
-        updateDisplay();
-        console.log("a is ",a, ", result is: ", a, "last click is ", lastClick);
+specialBtns.forEach(button =>{       // For "+/-" and "F!" buttons
+    button.addEventListener('click', () => {  
+        console.log(button.innerText);
+    
+        if(button.innerText === "+/-"){
+            displayValue = -(Number(displayValue));  // reverse sign
+        } else if (button.innerText === "F!"){
+            displayValue = factorial(displayValue);
+        };
+    
+        updateDisplay();    // Update display showing changes made to operand before computing a AND b based on lastClick 
+        b = displayValue;
+        compute(a, b, lastClick);
+        console.log("display value is", displayValue, "actual result is ", result, "result type", (typeof result), ", a is now", a, ", b is ", b, ", last click is ", lastClick, "clickcount is ", opClickCount);
     });
 });
 
-equalBtn.addEventListener('click', () => {
-    console.log("=");
-    displayValue = result.toString();
-    a = displayValue;
-    b = "";
-    opClickCount = 0;
+operatorBtns.forEach(button=> {             // For "=, +, - , * and /" buttons. 
+    button.addEventListener('click', () => {
+        // "="
+        if(button.innerText === "="){    
+            console.log("=");
+            a = displayValue;  
+            opClickCount = 0;   // resets operator click count 
+            console.log("displayValue is, ",displayValue, ",result is ", result, "a is now ", a,", b is now ", b, "lastClick: ", lastClick , "times clicked; ", "clickcount ;", opClickCount );
+        // +, - , * and /
+        } else {                      
+             opClickCount ++;
+             a = result;
+             console.log(lastClick, " is last click, ", "a is ",a, ", result is: ", a);
+        };
+        b = "";
+        displayValue = result.toString();
+        updateDisplay();
+        lastClick = button.innerText;
+    });
+});
+
+comaBtn.addEventListener('click', () => {
+    a = a.toString();
+    b = b.toString();
+    if (!a.includes(".") && lastClick === "a"){ 
+        a += ".";
+        displayValue = a;
+    } else if (!b.includes(".") && bClicked === "true"){  
+        b += ".";
+        displayValue = b;
+    }; 
+    console.log(". clicked, b is now", b,", a is now", a );
     updateDisplay();
-    lastClick = "=";
-    currentClick = "";
-    console.log("displayValue is, ",displayValue, ",result is ", result, "a is now ", a,", b is now ", b, "lastClick: ", lastClick , "times clicked; ", opClickCount );
-
-}); 
-
-plusBtn.addEventListener('click', () => {
-    lastClick = "+";
-    console.log("+");
 });
-
-multiplyBtn.addEventListener('click', () => {
-    lastClick = "*";
-    console.log("*");
-});
-
-minusBtn.addEventListener('click', () => {
-    lastClick = "-";
-    console.log("-");
-});
-
-divideBtn.addEventListener('click', () => {
-    lastClick = "/";
-    console.log("/");
-});
-
-factorialBtn.addEventListener('click', () => {
-    console.log("F!");
-    displayValue = factorial(displayValue);
-    updateDisplay();
-    b = displayValue;
-            if(lastClick === "+" ){
-                result = Number(a) + Number(b);
-                lastClick = "+";
-            } else if(lastClick === "*"){
-                console.log("*");
-                result = Number(a) * Number(b);
-                lastClick = "*";
-            } else if (lastClick === "-"){
-                console.log("-");
-                result = Number(a) - Number(b);
-                lastClick = "-";
-            } else if (lastClick === "/"){
-                if ( b === 0 ){
-                    displayValue = "ERROR"
-                };
-                console.log("/");
-                result = Number(a) / Number(b);
-                lastClick = "/";
-            } else {
-                result = displayValue;
-            };
-    console.log("display value is", displayValue, "actual result is ", result, ", a is now", a, ", b is ", b, ", last click is ", lastClick, "clickcount is ", opClickCount);
-    
-});
-
-
-signChangeBtn.addEventListener('click', () => {
-    console.log("+/-");
-    displayValue = -(Number(displayValue));
-    updateDisplay();
-    b = displayValue;
-            if(lastClick === "+" ){
-                result = Number(a) + Number(b);
-                lastClick = "+";
-            } else if(lastClick === "*"){
-                console.log("*");
-                result = Number(a) * Number(b);
-                lastClick = "*";
-            } else if (lastClick === "-"){
-                console.log("-");
-                result = Number(a) - Number(b);
-                lastClick = "-";
-            } else if (lastClick === "/"){
-                if ( b === 0 ){
-                    displayValue = "ERROR"
-                };
-                console.log("/");
-                result = Number(a) / Number(b);
-                lastClick = "/";
-            } else {
-                result = displayValue;
-            };
-    console.log("display value is", displayValue, "actual result is ", result, ", a is now", a, ", b is ", b, ", last click is ", lastClick, "clickcount is ", opClickCount);
-    
-});
-
-
-
 
 })();
 
 
-function factorial(num){
+
+//////////////////// FUNCTIONS : 
+
+function updateDisplay() {
+    const display = document.querySelector(".screenBody");
+    display.innerText = rounded(displayValue);
+};
+
+
+function factorial(num){ 
     if (num === 0){
         return 1;
       } else {
-        let fact = 1;                  // I have to predetermine fact = 1, otherwise I wont be able to access the variable in order to return it higher up  
+        // I have to pre-declare fact = 1, otherwise I wont be able to access the variable in order to return it higher up  
+        let fact = 1;                  
         for(let i = 1 ; i <= num ; i++){
           fact *= i;
         };
         return fact;
       };
 };
+             
 
-
-function compute (){
+function compute (a, b, lastClick){
+    a = Number(a);
+    b = Number(b);
     if(lastClick === "+" ){
-        result = Number(a) + Number(b);
+        result = a + b; 
         lastClick = "+";
     } else if(lastClick === "*"){
         console.log("*");
-        result = Number(a) * Number(b);
+        result = a * b;
         lastClick = "*";
     } else if (lastClick === "-"){
         console.log("-");
-        result = Number(a) - Number(b);
+        result = a - b;
         lastClick = "-";
     } else if (lastClick === "/"){
-        if ( b === 0 ){
-            displayValue = "ERROR"
-        };
         console.log("/");
-        result = Number(a) / Number(b);
+        result = a / b;
         lastClick = "/";
+    } else {
+        result = displayValue;
     };
+};
+
+
+function rounded(strResult){
+    if (strResult.toString().length > 11){
+       strResult = Number(strResult).toExponential();
+       strResult = Number(strResult).toPrecision(5);
+    };
+    return strResult;
 };
 
 
 
 
-
-
-/* // coma button code :  comaBtn.addEventListener('click', () =>{  //Refuse "." entry more than once. 
-        b += comaBtn.textContent;
-        comaBtn.disabled = true; 
-        let comaClicked = true;
-        console.log(b);
-        screenBody.textContent = b;
-    });
-    result.push(b); */ 
-
-
-
 /*
+Its the number input button that computes the calculation based on previous operator input 
+If b is 0, don't compute yet and result = a 
+if b is entered , a exists
 a is the first number input 
 b is the second number input
 
-whether I press "=" or "any other operator", compute the a and b calculation and update the a variable
+whether I press "=" or "any other operator", update last click and displayed value( which is the result previously calculated when a or b entered.)
 
 do I store input digits in a string or an array ? 
 
